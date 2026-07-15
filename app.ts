3ee19279fd02d621177f73ebfa8c179970dbc724
GabrielcTeles <gabrielctelless@outlook.com>
2026-07-15 10:49:47 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1362,7 +1362,7 @@ Alimentação, Transporte, Saúde, Educação, Lazer, Utilities ou Outros.
 `;
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
+      model: "gemini-3.1-flash-lite-preview",
       contents: {
         parts: [
           {
